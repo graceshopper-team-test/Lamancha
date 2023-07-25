d661bbc1eaa13c21e8b9a3e93846dfec1f939225
Asia Skinner <wolfe.asia@gmail.com>
2023-07-25 16:31:29 -0700

    Admin styling

--- client/app/components/AddProductForm.js
+++ client/app/components/AddProductForm.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addSingleProduct } from '../store/productSlice';
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { addSingleProduct } from "../store/productSlice";
+import "./AddProductForm.css";
 
 const AddProductForm = () => {
-  const [productName, setProductName] = useState('');
-  const [productImg, setProductImg] = useState('');
-  const [price, setPrice] = useState('');
-  const [description, setDescription] = useState('');
-  const [stock, setStock] = useState('');
+  const [productName, setProductName] = useState("");
+  const [productImg, setProductImg] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  const [stock, setStock] = useState("");
   const dispatch = useDispatch();
-
 
   const handleProductNameChange = (e) => {
     setProductName(e.target.value);
@@ -33,66 +33,68 @@ const AddProductForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newProduct = {productName, productImg, price,description,stock}
-    dispatch(addSingleProduct(newProduct))
+    const newProduct = { productName, productImg, price, description, stock };
+    dispatch(addSingleProduct(newProduct));
 
     // Clear the form fields after submission
-    setProductName('');
-    setProductImg('');
-    setPrice('');
-    setDescription('');
-    setStock('');
+    setProductName("");
+    setProductImg("");
+    setPrice("");
+    setDescription("");
+    setStock("");
   };
 
-  
-
   return (
-    <div>
+    <div className="addProductDiv">
       <h2>Add New Product</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="productName">Product Name:</label>
-          <input
-            type="text"
-            id="productName"
-            value={productName}
-            onChange={handleProductNameChange}
-            required
-          />
-          <label htmlFor="productImg">Product Image:</label>
-          <input
-            type="text"
-            id="productImg"
-            value={productImg}
-            onChange={handleProductImgChange}
-            required
-          />
-          <label htmlFor="price">Price:</label>
-          <input
-            type="number"
-            id="price"
-            value={price}
-            onChange={handlePriceChange}
-            required
-          />
-          <label htmlFor="description">Description:</label>
-          <textarea
-            id="description"
-            value={description}
-            onChange={handleDescriptionChange}
-            required
-          />
-          <label htmlFor="stock">Stock:</label>
-          <input 
-          type='number'
-            id="stock"
-            value={stock}
-            onChange={handleStockChange}
-            required
-          />
-        <button type="submit">Add Product</button>
-        </div>
-      </form>
+      <div className="addProductForm">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="productName">Product Name:</label>
+            <input
+              type="text"
+              id="productName"
+              value={productName}
+              onChange={handleProductNameChange}
+              required
+            />
+            <label htmlFor="productImg">Product Image:</label>
+            <input
+              type="text"
+              id="productImg"
+              value={productImg}
+              onChange={handleProductImgChange}
+              required
+            />
+            <label htmlFor="price">Price:</label>
+            <input
+              type="number"
+              id="price"
+              value={price}
+              onChange={handlePriceChange}
+              required
+            />
+            <label htmlFor="description">Description:</label>
+            <textarea
+              id="description"
+              value={description}
+              onChange={handleDescriptionChange}
+              required
+            />
+            <label htmlFor="stock">Stock:</label>
+            <input
+              type="number"
+              id="stock"
+              value={stock}
+              onChange={handleStockChange}
+              required
+            />
+            <button className="buttonAddProduct" type="submit">
+              Add Product
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- client/app/components/Admin.js
+++ client/app/components/Admin.js
@@ -2,9 +2,10 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { addToCart } from "../store/cartSlice";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import { deleteSingleProduct, fetchAllProducts } from "../store/productSlice";
 import AddProductForm from "./AddProductForm";
+import "./Admin.css";
 
 const Admin = () => {
   const products = useSelector((state) => state.products.allProducts);
@@ -16,41 +17,46 @@ const Admin = () => {
 
   return (
     <div>
-      <h1>HELLO FROM ADMIN DASHBOARD</h1>
-        <div className="productList">
+      <h1 className="headerAdmin">HELLO FROM ADMIN DASHBOARD</h1>
+      <div className="productList">
         <h2>Products List</h2>
         {products.length === 0 ? (
-            <p className="infoMessage">Loading...</p>
+          <p className="infoMessage">Loading...</p>
         ) : products.error ? (
-            <p className="infoMessage">Error: {products.error}</p>
+          <p className="infoMessage">Error: {products.error}</p>
         ) : (
-            <div className="productUL">
+          <div className="productUL">
             {products.map((product) => {
-                return (
+              return (
                 <div className="li" key={product.id}>
-                    <Link className="productName" to={`/products/${product.id}`}>
+                  <Link className="productName" to={`/products/${product.id}`}>
                     <h3>{product.name}</h3>
                     <img className="image" src={product.imageUrl} />
-                    </Link>
+                  </Link>
 
-                    <p className="productInfo">Price: ${product.price}</p>
-                    <p className="productInfo">Description: {product.details}</p>
-                    <p className="productInfo">Stock: {product.stock}</p>
-                    {/* <button
+                  <p className="productInfo">Price: ${product.price}</p>
+                  <p className="productInfo">Description: {product.details}</p>
+                  <p className="productInfo">Stock: {product.stock}</p>
+                  {/* <button
                     className="button"
                     onClick={() => handleAddToCart(product)}
                     >
                     Add To Cart
                     </button> */}
-                    <button onClick={() => dispatch(deleteSingleProduct(product.id))}>Delete</button>
-                    <form></form>
+                  <button
+                    className="buttonAdminList"
+                    onClick={() => dispatch(deleteSingleProduct(product.id))}
+                  >
+                    Delete
+                  </button>
+                  <form></form>
                 </div>
-                );
+              );
             })}
-            </div>
+          </div>
         )}
-        </div>
-        <AddProductForm />
+      </div>
+      <AddProductForm />
     </div>
   );
 };
