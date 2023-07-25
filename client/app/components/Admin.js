3a585342f88ef4fc9b76e494ab16394e8e69cfa0
Asia Skinner <wolfe.asia@gmail.com>
2023-07-25 16:51:12 -0700

    made a small change

--- client/app/components/Admin.js
+++ client/app/components/Admin.js
@@ -18,45 +18,52 @@ const Admin = () => {
   return (
     <div>
       <h1 className="headerAdmin">HELLO FROM ADMIN DASHBOARD</h1>
-      <div className="productList">
-        <h2>Products List</h2>
-        {products.length === 0 ? (
-          <p className="infoMessage">Loading...</p>
-        ) : products.error ? (
-          <p className="infoMessage">Error: {products.error}</p>
-        ) : (
-          <div className="productUL">
-            {products.map((product) => {
-              return (
-                <div className="li" key={product.id}>
-                  <Link className="productName" to={`/products/${product.id}`}>
-                    <h3>{product.name}</h3>
-                    <img className="image" src={product.imageUrl} />
-                  </Link>
+      <div className="listAndForm">
+        <div className="productList">
+          <h2>Products List</h2>
+          {products.length === 0 ? (
+            <p className="infoMessage">Loading...</p>
+          ) : products.error ? (
+            <p className="infoMessage">Error: {products.error}</p>
+          ) : (
+            <div className="productUL">
+              {products.map((product) => {
+                return (
+                  <div className="li" key={product.id}>
+                    <Link
+                      className="productName"
+                      to={`/products/${product.id}`}
+                    >
+                      <h3>{product.name}</h3>
+                      <img className="image" src={product.imageUrl} />
+                    </Link>
 
-                  <p className="productInfo">Price: ${product.price}</p>
-                  <p className="productInfo">Description: {product.details}</p>
-                  <p className="productInfo">Stock: {product.stock}</p>
-                  {/* <button
+                    <p className="productInfo">Price: ${product.price}</p>
+                    <p className="productInfo">
+                      Description: {product.details}
+                    </p>
+                    <p className="productInfo">Stock: {product.stock}</p>
+                    {/* <button
                     className="button"
                     onClick={() => handleAddToCart(product)}
                     >
                     Add To Cart
                     </button> */}
-                  <button
-                    className="buttonAdminList"
-                    onClick={() => dispatch(deleteSingleProduct(product.id))}
-                  >
-                    Delete
-                  </button>
-                  <form></form>
-                </div>
-              );
-            })}
-          </div>
-        )}
+                    <button
+                      className="buttonAdminList"
+                      onClick={() => dispatch(deleteSingleProduct(product.id))}
+                    >
+                      Delete
+                    </button>
+                    <form></form>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+        <AddProductForm />
       </div>
-      <AddProductForm />
     </div>
   );
 };
